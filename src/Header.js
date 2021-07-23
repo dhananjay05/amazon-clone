a5fc2a155a081c6b2a88d6938b179102aac97500
dhananjay05 <dhnanjay.05@gmail.com>
2021-07-24 03:23:51 +0530

    updated the logo stage

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      <img src="https://blog.logomyway.com/wp-content/uploads/2017/02/amazon-logo-1.jpg" alt="amazon_logo" />
+    </div>
+  )
+}
+
+export default Header
